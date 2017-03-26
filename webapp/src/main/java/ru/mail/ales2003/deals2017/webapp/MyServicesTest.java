c04d66b52d2a961fb2d1739b267568e4ab5c6089
Aliaksandr Vishneuski <ales_2003@mail.ru>
2017-03-27 00:27:37 +0300

    Data model & services are filled (without character_type, character_type2variant, item_variant, i18n). Also the implementing classes are proannotized

--- webapp/src/main/java/ru/mail/ales2003/deals2017/webapp/MyServicesTest.java
+++ webapp/src/main/java/ru/mail/ales2003/deals2017/webapp/MyServicesTest.java
@@ -10,10 +10,10 @@ public class MyServicesTest {
 	public static void main(String[] args) {
 
 		/*
-		 * контекст приложения ClassPathXmlApplicationContext
-		 * загружает определения компонентов из XML-файла,
-		 * расположенного в библиотеке классов (classpath), и обрабатывает файлы
-		 * с определениями контекстов как ресурсы
+		 * контекст приложения ClassPathXmlApplicationContext загружает
+		 * определения компонентов из XML-файла, расположенного в библиотеке
+		 * классов (classpath), и обрабатывает файлы с определениями контекстов
+		 * как ресурсы
 		 */
 
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("web-context.xml");
@@ -25,13 +25,14 @@ public class MyServicesTest {
 		// печатаем имя класса
 		System.out.println(iD.getClass());
 
-		// !!! когда имплементаций несколько, контекст не отдаст бин по интерфейсу,
+		// !!! когда имплементаций несколько, контекст не отдаст бин по
+		// интерфейсу,
 		// поэтому должныы указать класс, как здесь:
 		IItemDao impl = (IItemDao) context.getBean(ItemDaoImpl.class);
 		System.out.println(impl);
-		//какой-то тустринг, хочется лучше
+		// какой-то тустринг, хочется лучше
 		impl.toString();
-		
+
 		// интерфейс IItemDao не является бином, поэтому контекст его не
 		// содержит
 		System.out.println(context.containsBeanDefinition("iItemDao"));
