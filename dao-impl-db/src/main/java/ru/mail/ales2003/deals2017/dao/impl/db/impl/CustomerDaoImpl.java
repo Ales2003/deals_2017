4734960138ba802ca2a339c6f932bb8204b37d69
Aliaksandr Vishneuski <ales_2003@mail.ru>
2017-04-06 18:58:13 +0300

    Finished with Customer: dao, services

--- dao-impl-db/src/main/java/ru/mail/ales2003/deals2017/dao/impl/db/impl/CustomerDaoImpl.java
+++ dao-impl-db/src/main/java/ru/mail/ales2003/deals2017/dao/impl/db/impl/CustomerDaoImpl.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -20,6 +22,8 @@ import ru.mail.ales2003.deals2017.datamodel.Customer;
 
 @Repository
 public class CustomerDaoImpl implements ICustomerDao {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(CustomerDaoImpl.class);
 
 	@Inject
 	private JdbcTemplate jdbcTemplate;
@@ -58,32 +62,40 @@ public class CustomerDaoImpl implements ICustomerDao {
 
 	@Override
 	public Customer get(Integer id) {
+		final String READ_BY_ID_SQL = "select * from customer where id = ? ";
+
 		try {
-			return jdbcTemplate.queryForObject("select * from customer where id = ? ", new Object[] { id },
+			return jdbcTemplate.queryForObject(READ_BY_ID_SQL, new Object[] { id },
 					new BeanPropertyRowMapper<Customer>(Customer.class));
 		} catch (EmptyResultDataAccessException e) {
+			LOGGER.error("Error: customer with id = " + id + " don't exist in storage)", e);
 			return null;
 		}
 	}
 
 	@Override
 	public List<Customer> getAll() {
-		List<Customer> rs = jdbcTemplate.query("select * from customer ",
-				new BeanPropertyRowMapper<Customer>(Customer.class));
-		return rs;
+		try {
+			List<Customer> rs = jdbcTemplate.query("select * from customer ",
+					new BeanPropertyRowMapper<Customer>(Customer.class));
+			return rs;
+		} catch (EmptyResultDataAccessException e) {
+			LOGGER.error("Error: all customers don't exist in storage", e);
+			return null;
+		}
 	}
 
 	// =============UPDATE AREA===============
 
 	@Override
 	public void update(Customer entity) {
-		final String INSERT_SQL = "insert into customer " + "(first_name, patronymic, last_name, company_name,"
-				+ " address, phone_number,customer_group_id, manager_id)" + " values(?,?,?,?,?,?,?,?)";
+		final String UPDATE_SQL = "update customer set first_name = ?, patronymic = ?, last_name = ?, company_name = ?,"
+				+ " address = ?, phone_number = ?,customer_group_id = ?, manager_id = ? where id = ?";
 
 		jdbcTemplate.update(new PreparedStatementCreator() {
 			@Override
 			public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
-				PreparedStatement ps = connection.prepareStatement(INSERT_SQL, new String[] { "id" });
+				PreparedStatement ps = connection.prepareStatement(UPDATE_SQL, new String[] { "id" });
 				ps.setString(1, entity.getFirstName());
 				ps.setString(2, entity.getPatronymic());
 				ps.setString(3, entity.getLastName());
@@ -92,6 +104,7 @@ public class CustomerDaoImpl implements ICustomerDao {
 				ps.setString(6, entity.getPhoneNumber());
 				ps.setInt(7, entity.getCustomerGroupId());
 				ps.setInt(8, entity.getManagerId());
+				ps.setInt(9, entity.getId());
 				return ps;
 			}
 		});
--- services/src/main/java/ru/mail/ales2003/deals2017/services/impl/CustomerServiceImpl.java
+++ services/src/main/java/ru/mail/ales2003/deals2017/services/impl/CustomerServiceImpl.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import ru.mail.ales2003.deals2017.dao.impl.db.ICustomerDao;
@@ -13,43 +15,77 @@ import ru.mail.ales2003.deals2017.services.ICustomerService;
 @Service
 public class CustomerServiceImpl implements ICustomerService {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(CustomerServiceImpl.class);
+
 	@Inject
 	private ICustomerDao customerDao;
 
 	@Override
 	public Customer get(Integer id) {
-		return customerDao.get(id);
-	}
-
-	@Override
-	public void save(Customer customer) {
-		if (customer.getId() == null) {
-			System.out.println("Insert new Customer");
-			customerDao.insert(customer);
+		if (customerDao.get(id) == null) {
+			LOGGER.error("Error: customer with id = " + id + " don't exist in storage)");
+			return null;
 		} else {
-			System.out.println("Update existing Customer");
-			customerDao.update(customer);
+			Customer customer = customerDao.get(id);
+			LOGGER.info(
+					"Read one customer: id={}, first_name={}, patronymic={}, last_name={}, companyName={}, address={}, phoneNumber={}, customerGroupId={}, managerId={}",
+					customer.getId(), customer.getFirstName(), customer.getPatronymic(), customer.getLastName(),
+					customer.getCompanyName(), customer.getAddress(), customer.getPhoneNumber(),
+					customer.getCustomerGroupId(), customer.getManagerId());
+			return customer;
 		}
-
-	}
-
-	@Override
-	public void saveMultiple(Customer... customerArr) {
-		for (Customer customer : customerArr) {
-			save(customer);
-		}
-
 	}
 
 	@Override
 	public List<Customer> getAll() {
-		return customerDao.getAll();
+		if (customerDao.getAll() == null) {
+			LOGGER.error("Error: all customers don't exist in storage");
+			return null;
+		} else {
+			LOGGER.info("Read all customers");
+			return customerDao.getAll();
+		}
+	}
+
+	@Override
+	public void save(Customer customer) {
+		if (customer == null) {
+			LOGGER.error("Error: as the customer was sent a null reference");
+			return;
+		} else if (customer.getId() == null) {
+			customerDao.insert(customer);
+			LOGGER.info(
+					"Inserted new customer: id={}, first_name={}, patronymic={}, last_name={}, companyName={}, address={}, phoneNumber={}, customerGroupId={}, managerId={}",
+					customer.getId(), customer.getFirstName(), customer.getPatronymic(), customer.getLastName(),
+					customer.getCompanyName(), customer.getAddress(), customer.getPhoneNumber(),
+					customer.getCustomerGroupId(), customer.getManagerId());
+		} else {
+			customerDao.update(customer);
+			LOGGER.info(
+					"Updated customer: id={}, first_name={}, patronymic={}, last_name={}, companyName={}, address={}, phoneNumber={}, customerGroupId={}, managerId={}",
+					customer.getId(), customer.getFirstName(), customer.getPatronymic(), customer.getLastName(),
+					customer.getCompanyName(), customer.getAddress(), customer.getPhoneNumber(),
+					customer.getCustomerGroupId(), customer.getManagerId());
+		}
+	}
+
+	@Override
+	public void saveMultiple(Customer... customerArray) {
+		for (Customer customer : customerArray) {
+			LOGGER.debug("Inserted new customer from array: " + customer);
+			save(customer);
+		}
+		LOGGER.info("Inserted customers from array");
 	}
 
 	@Override
 	public void delete(Integer id) {
-		customerDao.delete(id);
-
+		if (id == null) {
+			LOGGER.error("Error: as the id was sent a null reference");
+			return;
+		} else {
+			customerDao.delete(id);
+			LOGGER.info("Deleted customer by id: " + id);
+		}
 	}
-
 }
