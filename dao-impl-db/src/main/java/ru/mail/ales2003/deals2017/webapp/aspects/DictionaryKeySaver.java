84daf7ff4760173bb1cec709c0368ae4ff0fb4fa
Aliaksandr Vishneuski <ales_2003@mail.ru>
2017-04-28 00:01:45 +0300

    no message

--- dao-impl-db/src/main/java/ru/mail/ales2003/deals2017/webapp/aspects/DictionaryKeySaver.java
+++ dao-impl-db/src/main/java/ru/mail/ales2003/deals2017/webapp/aspects/DictionaryKeySaver.java
@@ -1,10 +1,17 @@
 package ru.mail.ales2003.deals2017.webapp.aspects;
 
-import org.aspectj.lang.annotation.Aspect;
-
-@Aspect
+//@Aspect
 public class DictionaryKeySaver {
 
-	
-	
+	// @Pointcut("execution(*
+	// ru.mail.ales2003.deals2017.dao.db.impl.AbstractDaoImplDb.insert(..))")
+	public void fillingTables() {
+
+	}
+
+	// @After("fillingTables()")
+	public void insertKeysInDb() {
+		System.out.println("=======================================USE ASPECTS===========================");
+	}
+
 }
